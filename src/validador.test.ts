047c32e38535151abc2ab90e0edaa8333390034c
IvooBSJ <bluerewhd@gmail.com>
2026-06-18 15:09:53 -0300

    vuelta a lo correcto

--- src/validador.test.ts
+++ src/validador.test.ts
@@ -5,7 +5,7 @@ describe("Validador de Email", () => {
         expect(validarEmail("usuario@gmail.com")).toBe(true);
     });
     test("email sin arroba", () => {
-        expect(validarEmail("usuariogmail.com")).toBe(true);
+        expect(validarEmail("usuariogmail.com")).toBe(false);
     });
     test("email vacío", () => {
         expect(validarEmail("")).toBe(false);
